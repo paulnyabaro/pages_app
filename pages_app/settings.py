d84dee3961a6869d39ca34c3de8d70bca4e2b14c
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-30 06:25:15 +0300

    Registered templates

--- pages_app/settings.py
+++ pages_app/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Registering templates
+TEMPLATES = [
+    {
+        'DIRS': [BASE_DIR / 'templates'], 
+    }
+]
