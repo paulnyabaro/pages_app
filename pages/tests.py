8635dc0c8575991d9288447510fd96cfe40abc15
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-31 08:05:53 +0300

    Testing template used and if template contains certain data

--- pages/tests.py
+++ pages/tests.py
@@ -12,6 +12,14 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response,'<h1>This is the homepage</h1>')
+
 class AboutpageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get('/about/')
@@ -20,3 +28,11 @@ class AboutpageTests(SimpleTestCase):
     def test_url_available_by_name(self):
         response = self.client.get(reverse('about'))
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'about.html')
+
+    def test_template_content(self):
+        response = self.client.get(reverse('about'))
+        self.assertContains(response,'<h1>This is the about page</h1>')
