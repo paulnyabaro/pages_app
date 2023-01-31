83fc7835d5fe4ebb1fa898bf52ec61366fc63752
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-31 07:54:38 +0300

    Testing if about page url returns a status code of 200

--- pages/tests.py
+++ pages/tests.py
@@ -7,3 +7,7 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
+class AboutpageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200)
