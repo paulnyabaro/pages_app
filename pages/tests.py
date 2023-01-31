6ca9d9c9dcab27d08413e72616299b9ea3601e62
paulnyabaro <paulnyabaro1@gmail.com>
2023-01-31 07:53:37 +0300

    Testing if homepage url returns a status code of 200

--- pages/tests.py
+++ pages/tests.py
@@ -1,4 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase # db involved
 from django.test import SimpleTestCase # When no db is needed
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
